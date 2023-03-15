070d10a87ab94b8e4905d05b2cc5d9215dba18f5
Sarah Alexander <sjalex78@gmail.com>
2023-03-15 17:20:03 +1100

    [config] add GH Action specific WebDriver options

--- spec/support/capybara.rb
+++ spec/support/capybara.rb
@@ -6,6 +6,21 @@ require "capybara-inline-screenshot/rspec"
 
 Capybara.javascript_driver = :selenium_chrome
 
+Capybara.register_driver :selenium_chrome do |app|
+  options = Selenium::WebDriver::Chrome::Options.new
+  if ENV.fetch("GITHUB_ACTIONS", nil) == "true"
+    options.add_argument("--headless")
+    options.add_argument("--disable-gpu")
+  end
+
+  args = {browser: :chrome}
+  args[:options] = options if options
+  Capybara::Selenium::Driver.new(
+    app,
+    **args,
+  )
+end
+
 Capybara::Screenshot.register_driver(:selenium_chrome) do |driver, path|
   driver.browser.save_screenshot(path)
 end
