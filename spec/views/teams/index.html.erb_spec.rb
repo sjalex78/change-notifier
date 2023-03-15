d4e232bffd6c4b05776f84fe37066e5b54416ecb
Sarah Alexander <sjalex78@gmail.com>
2023-03-15 15:57:57 +1100

    [TDD] adding name input label

--- spec/views/teams/index.html.erb_spec.rb
+++ spec/views/teams/index.html.erb_spec.rb
@@ -5,6 +5,18 @@ require "rails_helper"
 describe "teams/index.html.erb" do
   it "has a title" do
     render
-    expect(rendered).to have_content "Add my basketball team"
+    h1_heading = Capybara.string(rendered).find("h1")
+    expect(h1_heading.text).to eq "Add my basketball team"
+  end
+
+  it "input called name" do
+    render
+    name_field = Capybara.string(rendered).find('label[for="team_name"]').find(:xpath, "..")
+    expect(name_field.find("label").text).to eq "Name"
+    expect(name_field.find("label")["for"]).to eq "team_name" # for screen readers and page navigation
+    pending "waiting for input field to be added"
+    expect(name_field.find("input")["id"]).to eq "team_name" # for screen readers and page navigation
+    expect(name_field.find("input")["type"]).to eq "text"
+    expect(name_field.find("input")["name"]).to eq "team[name]"
   end
 end
